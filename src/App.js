9db94d7fc9be8d4e1f58dfb5f8c28fcc3f8a7564
mahesh <pmahesh493605@gmail.com>
2024-12-08 16:54:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,222 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='app-container'>
+        <header className="header">
+          <div className="container">
+            <img src="https://res.cloudinary.com/ddsn9feta/image/upload/v1733630883/image_bf9y28.png" alt="Elbrit Logo" className="logo"/ >
+          </div>
+
+          
+    </header>
+
+    <section className="hero">
+        <div className="container hero-content">
+          <h1 className='hero-heading'>Essential Vitamins</h1>
+          <div className='hero-container'>
+             <div className='hero-top-left-container'>
+                <p className='hero-top-left-para'>Online Medical Supplies</p>
+                <h4 className='hero-top-left-head'>Get Your Vitamins & Minerals</h4>
+                <button className='button'>EXPLORE</button>
+             </div>
+             <div className='tonic-container'>
+              <img src='https://s3-alpha-sig.figma.com/img/6dd8/7f0a/f73aa0189cad884f47381dd92205b7c0?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=V21VVDmU7JxcRB76OOhoAt9ZJzhck8hR9U3L1UvokjwzK916BF2uOVElrkcxN5YP-si~qFGW9mXybipDrvdR9inQNdWLrDUUFadDcDwhwDJ4adJ0UfkwYRnZT-0b2Y9w29vguT4lAW1HMCi344hAaPojIhdUTUnuzzFnqR8X2VtFZXtBgZJYDWU5w5bRDHt1Nlxenbc59CkLE-RpPwl-8Wt6uz8rwl5~X6nDY-R0fyUzHYIp51B4LQF2W32WHBANFHGkAREYP66qtVWdYPpqIVAoba2zRrWozjVW~~unoMkhN0MSl-kap8oS2~IXt2HMzEYndt7tu2DsSbjQue1p9g__' alt='img' className='tonic'/>
+             </div>
+             <div className='hero-top-right-container'>
+                <div className='hero-side-section'>
+                  <img src='https://s3-alpha-sig.figma.com/img/1d76/fc2a/470ec862c44115964979f90b5cb3a94e?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=l8br3l5hvS1vnQy3WMDDXmvwBbAoZGBF~14OpoaLciq8S325-GN8~oF56m4ULopQlNJDHShbY78MmUGsjZKiZYOrAbdXVyr3HUSNpFdJtbKkVtYhvAUvHp6ghUpHNKNf2H0EhxgXQAdX5jDhFvmlnt6~wcvCWsUCjPhetY5usDdwSlgaddrZxhaKIuaDy8aQ9FqvG5MWZKQTnkBeWZlih89WfilN3Vkv0Iu6w2x8TmzehqQWS-GJFBXyOBMB1X-NSi2AkO4VTnN6MfPeNS-P8DlWm6JYTg4RHFY2CqAtNMenk5f9jmbl4lN-RE-Zn8XYlohyzBU~XMQB~2rnXotnfw__' alt='img' className='vitamin'/>
+                  <div>
+                     <h1 className='hero-top-right-head'>Vitamins</h1>
+                     <p className='hero-top-right-para'>Increased Vitamins and minerals in your diet</p>
+                  </div>
+                </div>
+                <div className='hero-side-section'>
+                  <img src='https://s3-alpha-sig.figma.com/img/c0ae/6abf/9917c0af12dcc49a9c0130d3e68e04b1?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dx9BsMA39Mxa2Odn8qXpjgqLgJyWy~dNaB6DWyuRYIcHKphjSwmcU7SAkxdZ7PI3POB9Uyzc7nMnVoElXMw8CgPzRcSmULQkpHMiGrYbDG~Q8DlnJE-hE3NelQMqZNZdOpuTPBcD2GN2o45uUYVUedoMKhr7BJjjq4Bn6KTkZ8kgzrpS0AEGU7GL1TEkF3n80lm8y4WXbhcWvQ3t7sDsmGyXpFYztMuybNF4rLHwuGcQ0GYWpRQa9~jb1uyEynJGetiEPYQmrQaagS2tvkDhCD0TZ~0YZqnSdYFqi3IGnBJH3YiIqdWxdwfe6MEQgJ~EpmW6GtjA0PRSMQBujKvDvg__' alt='img' className='vitamin'/>
+                  <div>
+                     <h1 className='hero-top-right-head'>Weight Loss</h1>
+                     <p className='hero-top-right-para'> Weight Loss 
+                     Find scientifically proven solutions</p>
+                  </div>
+                </div>
+                <div className='hero-side-section'>
+                  <img src='https://s3-alpha-sig.figma.com/img/ad6c/6ad1/2116d12f8d75befd05e66d41425cb402?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dWc~OffEuoFYOrTG-j6GYpo7NN-Y4O29eyv2w4PY1~ndA0~r8ZJogsmhFEggIbt7DOo2KwWhgGJdYo2RZL04W151-jnCLmEga1NVbZadhi07US-q8oBLAeJ~QkANDAPnzlpL2f4o89~21nLtKDEZYITd4HvG1YF80DhhYtubj05OA4f6hmeprvUIFnJ~MUPSU6ywuc63KZhOGnePDiNYC6wVcIODrgb73yX8-FwFmjTpn690lIb5n2U~-GfjI6I9rTCh950Vdy-UlQ7qO7hG4meji~V9EAGgSZ5HoUvIxN9zicR~Mi2mncb8ECgPKe10P04yn6HfTmecbXP5Ox9lTA__' alt='img' className='vitamin'/>
+                  <div>
+                     <h1 className='hero-top-right-head'>Functional Foods</h1>
+                     <p className='hero-top-right-para'>Functional Foods
+                     From protein powers to baby formula</p>
+                  </div>
+                </div>
+             </div>
+          </div>
+        </div>
+  </section>
+
+
+  {/* Benefits Section */}
+    <section className='benefits'>
+      <div className='benefits-container'>
+          <div className='benefit'>
+            <div className='benefit-image-container'>
+                <img src='https://res.cloudinary.com/ddsn9feta/image/upload/v1733635457/Group-8111.svg_echezg.png' alt='benefit' className='benefit-image'/>
+            </div>
+                <h3 className='benefit-head'>Clinically Studied</h3>
+                <p className='benefit-para'>All products that we  offer have undergone lab and safety tests</p>
+          </div>
+          <div className='benefit'>
+            <div className='benefit-image-container'>
+                <img src='https://res.cloudinary.com/ddsn9feta/image/upload/v1733635457/Group-8110.svg_cacpzs.png' alt='benefit' className='benefit-image'/>
+            </div>
+                <h3 className='benefit-head'>Vegetarian Friendly</h3>
+                <p className='benefit-para'>We have a wide selection of vegetarian products to meet your needs</p>
+          </div>
+          <div className='benefit'>
+            <div className='benefit-image-container'>
+                <img src='https://res.cloudinary.com/ddsn9feta/image/upload/v1733635457/Group-8117.svg_izxqp8.png' alt='benefit' className='benefit-image'/>
+            </div>
+                <h3 className='benefit-head'>Made In India</h3>
+                <p className='benefit-para'>Shop local and explore health products made right here in India</p>
+          </div>
+      </div>
+      <div className='benefits-container'>
+          <div className='benefit'>
+            <div className='benefit-image-container'>
+                <img src='https://res.cloudinary.com/ddsn9feta/image/upload/v1733635457/Group-8114.svg_xqk7lh.png' alt='benefit' className='benefit-image'/>
+            </div>
+                <h3 className='benefit-head'>Free Shopping</h3>
+                <p className='benefit-para'>We deliver to your door with no shipping costs on your orders</p>
+          </div>
+          <div className='benefit'>
+            <div className='benefit-image-container'>
+                <img src='https://res.cloudinary.com/ddsn9feta/image/upload/v1733635457/Group-8115.svg_qp4iv2.png' alt='benefit' className='benefit-image'/>
+            </div>
+                <h3 className='benefit-head'>No Risk</h3>
+                <p className='benefit-para'>We ensure that all products are safe and within their use-by date</p>
+          </div>
+          <div className='benefit'>
+            <div className='benefit-image-container'>
+                <img src='https://res.cloudinary.com/ddsn9feta/image/upload/v1733635457/Group-8116.svg_bf08u3.png' alt='benefit' className='benefit-image'/>
+            </div>
+                <h3 className='benefit-head'>GMO Free</h3>
+                <p className='benefit-para'>Natural, no modified products and derivatives for those who need it</p>
+          </div>
+      </div>
+    </section>
+
+
+    <section className='ingredients-container'>
+      <div className='better-ingredients'>
+            <p className='ingredients-para'>INGREDIENTS</p>
+            <h2 className='ingredients-head'>Better Ingredients</h2>
+            <p className='ingredients-desc'>Only the best when you choose products offered on our platform - high-quality ingredients for high quality products!</p>
+      </div>
+      <div className='ingredients-sub-container-1 ingredients-sub-container'>
+        <div>
+          <h4 className='ingredients-sub-container-head'>Vitamin C</h4>
+          <p className='ingredients-sub-container-para'>Vitamin C as ascorbic acid</p>
+        </div>
+           <button className='see-more-button'>SEE MORE</button> 
+      </div>
+      <div className='ingredients-sub-container-2 ingredients-sub-container'>
+        <div>
+          <h4 className='ingredients-sub-container-head'>Vitamin B3</h4>
+          <p className='ingredients-sub-container-para'>Niacin for healthy gut and skin</p>
+        </div>
+           <button className='see-more-button'>SEE MORE</button> 
+      </div>
+      <div className='ingredients-sub-container-3 ingredients-sub-container'>
+        <div>
+          <h4 className='ingredients-sub-container-head'>Magnesium</h4>
+          <p className='ingredients-sub-container-para'>Boost energy and support muscle function</p>
+        </div>
+           <button className='see-more-button'>SEE MORE</button> 
+      </div>
+      <div className='ingredients-sub-container-4 ingredients-sub-container'>
+        <div>
+          <h4 className='ingredients-sub-container-head'>Hyaluronic Acid</h4>
+          <p className='ingredients-sub-container-para'>For smooth, supple and soft skin!</p>
+        </div>
+           <button className='see-more-button'>SEE MORE</button> 
+      </div>
+      <div className='ingredients-sub-container-5 ingredients-sub-container'>
+        <div>
+          <h4 className='ingredients-sub-container-head'>Lactobacillus</h4>
+          <p className='ingredients-sub-container-para'>Invigorate your gut microbiome</p>
+        </div>
+           <button className='see-more-button'>SEE MORE</button> 
+      </div>
+      <img src='https://res.cloudinary.com/ddsn9feta/image/upload/v1733645421/div.elbzotech-banner-info-thumb_p8n9yx.png' alt='ingredients' className='ingredients-image'/>
+    </section>
+
+
+      <>
+      <p className='blog'>OUR BLOG</p>
+      <h3 className='latest-head'>Latest News</h3>
+
+      <section className='Latest-news-container'>
+      <div className='one'>
+          <div className='first'>
+              <div className='con-1'><h5 className='covid'>The Covid-19 Epidemic In 2022 is Back</h5></div>
+              <div className='con-2'><h5 className='covid'>The Covid-19 Epidemic In 2022 is Back</h5></div>
+          </div>
+          <div className='second'>
+              <div className='con-3'><h5 className='covid'>The Covid-19 Epidemic In 2022 is Back</h5></div>
+              <div className='con-4'><h5 className='covid'>The Covid-19 Epidemic In 2022 is Back</h5></div>
+          </div>
+      </div>
+      <div className='one'>
+          <div className='first'>
+              <div className='con-1'><h5 className='covid'>The Covid-19 Epidemic In 2023 is Back</h5></div>
+              <div className='con-2'><h5 className='covid'>The Covid-19 Epidemic In 2023 is Back</h5></div>
+          </div>
+          <div className='second'>
+              <div className='con-3'><h5 className='covid'>The Covid-19 Epidemic In 2023 is Back</h5></div>
+              <div className='con-4'><h5 className='covid'>Hac hendrerit mus nons semper suspendisse</h5></div>
+          </div>
+      </div>
+    </section>
+      </>
+
+      <footer className='footer-container'>
+        <div className='details'>
+        <div className='details-address-container'>
+                <img className='footer-image' src='https://res.cloudinary.com/ddsn9feta/image/upload/v1733653608/Component-2.svg_fdhy7n.png' alt='footer-img'/>
+                <div className='details-con'>
+                     <h4>Phone Number</h4>
+                     <h4>+974 3118 1843</h4>
+                </div>
+            </div>
+            <div className='details-address-container'>
+                <img className='footer-image' src='https://res.cloudinary.com/ddsn9feta/image/upload/v1733653608/Component-2-1.svg_vwqa8q.png' alt='footer-img'/>
+                <div className='details-con'>
+                     <h4>Email Address</h4>
+                     <h4>Elbrithcqhr@gmail.com</h4>
+                </div>
+            </div>
+            <div className='details-address-container'>
+                <img className='footer-image' src='https://res.cloudinary.com/ddsn9feta/image/upload/v1733653608/Component-2-2.svg_ltxs96.png' alt='footer-img'/>
+                <div className='details-con'>
+                     <h4>Office Location</h4>
+                     <h4> Ambassador Street, Zone 61,</h4>
+                </div>
+            </div>
+        </div>
+           
+
+            <div className='icon-desc-footer'>
+              <img src='https://res.cloudinary.com/ddsn9feta/image/upload/v1733630883/image_bf9y28.png' alt='' className='logo'/>
+              <h4> Your health, physical and emotional well-being is important to us. We are always by your side and have made it even easier for you to find the necessary vitamins. </h4>
+            </div>
+
+            <div className='bottom-footer'>
+                  <img src='https://res.cloudinary.com/ddsn9feta/image/upload/v1733654610/Frame_nn2qld.png' alt='loc' className='location'/>
+                  <p>Elbrit Life Sciences Private Limited. C20, BKC, G Block, Mumbai 400051</p>
+            </div>
+      </footer>
+    
+
+    </div>
+  );
+}
+
+export default App;
